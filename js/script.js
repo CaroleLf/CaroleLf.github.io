9d3d8568e741ef78a7430cfd8903835bd391a912
CaroleLf <carole.56leflohic@gmail.com>
2022-12-22 23:55:09 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,81 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('open');
+};
+
+const sr = ScrollReveal ({
+	distance: '65px',
+	duration: 2600,
+	delay: 450,
+	reset: true
+});
+
+
+
+
+sr.reveal('.me-text',{delay:200, origin:'top'});
+sr.reveal('.me-Personal',{delay:200,origin:'top'});
+sr.reveal('.me-img',{delay:450, origin:'top'});
+sr.reveal('.icons',{delay:500, origin:'left'});
+sr.reveal('.scroll-down',{delay:500, origin:'right'});
+sr.reveal('.work', {delay:200,origin:'right'})
+sr.reveal('.More',{delay:200,origin:'left'});
+
+
+/***** MiXITUUP FILTER  **************************/
+var containerEl = document.querySelector('.work_container');
+var mixer;
+
+if (containerEl) {
+    mixer = mixitup(containerEl, {
+		selectors :  {
+			target : '.work__card'
+		},
+         animation : {
+			duration : 300
+		}
+    });
+}
+
+
+
+/*** LINK ACTIVE WORK  ********/
+
+const linkWork  = document.querySelectorAll('.work_item');
+
+function activeWork(){
+	linkWork.forEach(l => l.classList.remove('active-work'))
+	this.classList.add('active-work')
+}
+
+linkWork.forEach(l=> l.addEventListener("click",activeWork))
+
+
+
+/*** PopUP ***/
+
+document.addEventListener("click", (e)=> {
+	if(e.target.classList.contains("work__button")){
+		togglePortfolioPopup();
+		portfolioItemDetails(e.target.parentElement);
+
+	}
+})
+
+
+function togglePortfolioPopup(){
+	document.querySelector(".portfolio__popup").classList.toggle("open");
+}
+
+document.querySelector(".portfolio__popup-close").addEventListener("click", togglePortfolioPopup)
+
+
+function portfolioItemDetails(portfolioItem){
+	document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".work_img").src;
+	document.querySelector(".portfolio__popup-subtitle span ").innerHTML = portfolioItem.querySelector(".work_title").innerHTML;
+	document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML;
+
+}
