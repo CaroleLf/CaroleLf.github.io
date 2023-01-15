8d321a92f1b49d344a9bda035c7753a58ba3c755
CaroleLf <carole.56leflohic@gmail.com>
2023-01-15 21:29:23 +0100

    change font-size navbar

--- js/script.js
+++ js/script.js
@@ -22,7 +22,6 @@ sr.reveal('.me-img',{delay:450, origin:'top'});
 sr.reveal('.icons',{delay:500, origin:'left'});
 sr.reveal('.scroll-down',{delay:500, origin:'right'});
 sr.reveal('.work', {delay:200,origin:'right'})
-sr.reveal('.More',{delay:100,origin:'left'});
 
 
 /***** MiXITUUP FILTER  **************************/
